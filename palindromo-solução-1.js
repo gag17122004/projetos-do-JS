d27f820e3e9e997e504153396ad7e9288f6335c9
Guilherme Anastacio Gerlach <120632670+gag17122004@users.noreply.github.com>
2023-02-15 13:53:38 -0300

    Update palindromo-solução-1.js

--- palindromo-solução-1.js
+++ palindromo-solução-1.js
@@ -1,10 +1,28 @@
 //solução 1
 function verificaPalindromo(string) {
-    if (!string) return;
+    if (!string) return"string inexistente";
 
   return string.split("").reverse().join("") === string;
 }
 
 console.log(verificaPalindromo(""));
+
+
+
+//solução 2
+function verificaPalindromo2(string) {
+  if (!string) return"string inexistente";
+
+  for(let i = 0; i < string.length / 2; i++) {
+    if(string[i] !== string[string.length - 1 - i]) {
+     return false;
+    }
+  }
+
+  return true;
+}
+
+console.log(verificaPalindromo2(""));
+
 
 if string seja um palindromo ele ira devolver'true', else ira devolver 'false'; 
