bdf815c40f1befc0c403ca24e58b844655a34b30
Guilherme Anastacio Gerlach <120632670+gag17122004@users.noreply.github.com>
2023-03-24 15:14:27 -0300

    Add files via upload

---
+++ main.mjs
@@ -0,0 +1,5 @@
+import { mostraCidade, mostraIdade, mostraHobby } from "./funcoes.mjs";
+
+console.log(mostraIdade('Guilherme', '18'));
+console.log(mostraCidade('Guilherme', 'São Carlos'));
+console.log(mostraHobby('Guilherme', 'estudar'));
