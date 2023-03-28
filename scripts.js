f1ef309b4c106b76a2a8fc6130d517156a2c78b1
Guilherme Anastacio Gerlach <120632670+gag17122004@users.noreply.github.com>
2023-03-28 15:09:47 -0300

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,22 +1,34 @@
-const BASE_URL = 'https://thatcopy.pw/catapi/rest/';
-const catBtn = document.getElementById('change-cat');
-const catImg = document.getElementById('cat');
-
-const getCats = async () => {
-    try {
-        const data = await fetch(BASE_URL);
-        const json = await data.json();
-
-        return json.webpurl;
-    } catch(e) {
-        console.log(e.message);
-    }
-};
-
-const loadImg = async () => {
-    catImg.src = await getCats();
+function changeMode() {
+    changeClasses();
+    changeText();
 }
 
-catBtn.addEventListener('click', loadingImg);
+function changeClasses() {
+    button.classList.toggle(darkModeClass);
+    h1.classList.toggle(darkModeClass);
+    body.classList.toggle(darkModeClass);
+    footer.classList.toggle(darkModeClass);
+}
 
-loadImg();
+function changeText() {
+    const lightMode = "Light Mode";
+    const darkMode = "Dark Mode";
+
+    if(button.classList.contains(darkModeClass)) {
+        button.innerHTML = LightMode;
+        h1.innerHTML = darkMode + "ON";
+        return;
+    }
+
+    button.innerHTML = darkMode;
+    h1.innerHTML = lightMode + "ON"; 
+
+}
+
+const darkModeClass = 'dark-mode';
+const button = document.getElementByld('mode-selector');
+const h1 = document.getElementByld('page-title');
+const body = document.getElementsByTagName('body')[0];
+const footer = document.getElementsByTagName('footer')[0];
+
+button.addEventListener('click', changeMode);
